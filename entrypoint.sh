eb958083dffadc861ff4c7e1d6b1fd095f562080
Théo Mathieu <theo.mathieu@bhvr.com>
2017-01-25 16:00:35 -0500

    fix: remove aoc until angular 2.3.1 is supported

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ fi
 
 if [ $1 = "prod-browser" ]; then
     cp src/env/prod.ts src/env/main.ts
-    npm run ionic:build --prod
+    npm run ionic:build #--prod
 fi
 if [ $1 = "test" ]; then
     cp src/env/dev.ts src/env/main.ts
